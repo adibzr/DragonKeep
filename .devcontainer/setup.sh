39d5e0081605988ade18298fed3e75a966a4cee0
Adib Raed <adibraed1@gmail.com>
2025-04-09 14:25:27 +0000

    added vite boiler plate

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,11 +2,12 @@
 
 if [ ! -d "my-app" ]; then
   echo "Creating Vite app..."
-  npm create vite@latest my-app --template react-ts -- --yes
+  export NPM_CONFIG_YES=true
+  yes | npm create vite@latest my-app -- --template react-ts
 else
   echo "Vite app already exists."
 fi
 
 cd my-app
 echo "Installing dependencies..."
-npm install
+npm install --yes
